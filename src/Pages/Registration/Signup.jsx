4c5030d4e6d9ea41b0ffca1139f9c458f55519e7
rachit <curiousrachit.26@gmail.com>
2023-11-14 19:55:13 +0530

    created a signup page

--- src/Pages/Registration/Signup.jsx
+++ src/Pages/Registration/Signup.jsx
@@ -2,6 +2,10 @@ import React, { useContext, useState } from 'react';
 import { Link } from 'react-router-dom';
 import myContext from '../../Contexts/Data/MyContext';
 import { toast } from 'react-toastify';
+import { auth, fireDB } from '../../FireBase/FirBase';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { Timestamp, addDoc, collection } from 'firebase/firestore';
+// import { Timestamp, addDoc, collection } from 'firebase/firestore';
 const Signup = () => {
   const [name, setName] = useState('');
 
@@ -11,10 +15,10 @@ const Signup = () => {
 
 
   // ..................................using loading through context method........./
-  
-  const context= useContext(myContext);
-  const {loading,setLoading}=context;
-  
+
+  const context = useContext(myContext);
+  const { loading, setLoading } = context;
+
   const handleSignup = (e) => {
     e.preventDefault();
     // Implement your signup logic here
@@ -22,33 +26,72 @@ const Signup = () => {
 
 
 
-// ......................signup function..........
+  // ......................signup function..........
 
 
-const signup=()=>{
+  const signin = async () => {
 
-if(name==="" || email==="" || password==="" ) {
-  return  toast.error("all fields are required")
-} 
+    if (name === "" || email === "" || password === "") {
+      return toast.error("all fields are required")
+    }
 
-  // console.log(name,email,password)
-}
+
+
+    try {
+
+
+      const users = await createUserWithEmailAndPassword(auth, email, password);
+      console.log(users);
+
+
+            // console.log(users)
+
+            const user = {
+              name: name,
+              uid: users.user.uid,
+              email: users.user.email,
+              time : Timestamp.now()
+          }
+          const userRef = collection(fireDB, "users")
+          await addDoc(userRef, user);
+          toast.success("Signup Succesfully")
+          setName("");
+          setEmail("");
+          setPassword("");
+
+
+    
+    
+    
+    
+    }
+
+
+
+
+
+
+    catch (error) {
+      console.log(error);
+    }
+    // console.log(name,email,password)
+  }
 
   return (
     <div className="flex h-screen bg-gray-200">
       <div className="m-auto p-6 bg-white rounded-lg shadow-md w-96">
         <h2 className="text-2xl font-semibold text-center">Sign Up</h2>
         <form className="mt-6" onSubmit={handleSignup}>
-         
-         
+
+
           <div className="mb-4">
-          
-          
-          
-          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="name">
+
+
+
+            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="name">
               Name
             </label>
-           
+
             <input
               className="w-full px-3 py-2 border rounded-md focus:outline-none focus:border-blue-400"
               type="text"
@@ -58,10 +101,10 @@ if(name==="" || email==="" || password==="" ) {
               onChange={(e) => setName(e.target.value)}
               required
             />
-          
-          
-          
-          
+
+
+
+
             <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
               Email
             </label>
@@ -98,7 +141,7 @@ if(name==="" || email==="" || password==="" ) {
           <button
             className="w-full bg-blue-500 text-white font-semibold p-2 rounded-md hover:bg-blue-600"
             type="submit"
-            onClick={signup}
+            onClick={signin}
           >
             Sign Up
           </button>
