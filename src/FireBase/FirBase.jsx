7dd42f07cd6f627773a220d22c676f9e1e0e553d
rachit <curiousrachit.26@gmail.com>
2023-11-14 19:54:39 +0530

    added my firebase

--- src/FireBase/FirBase.jsx
+++ src/FireBase/FirBase.jsx
@@ -1,3 +1,7 @@
+
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
@@ -15,3 +19,12 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+const fireDB = getFirestore(app);
+const auth = getAuth(app)
+export {fireDB,auth } ;
+
+
+
+
+
